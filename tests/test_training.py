61bcd188f47682b171d72da6c13ed0a16fb3e7a3
darshan8970 <darshangowda709024@gmail.com>
2026-08-15 11:24:43 +0530

    test: validate main pipeline entry point

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,5 +1,5 @@
-from src.train import train_and_tune_models
+import main
 
 
-def test_training_function_is_available():
-    assert callable(train_and_tune_models)
+def test_main_module_imports():
+    assert callable(main.main)
